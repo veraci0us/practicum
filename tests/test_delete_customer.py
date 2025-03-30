82210a7682e680d01a9aa6ac811137c953834a4e
veraci0us <200478683+veraci0us@users.noreply.github.com>
2025-03-31 00:16:54 +0300

    add allure

--- tests/test_delete_customer.py
+++ tests/test_delete_customer.py
@@ -1,18 +1,25 @@
 from pages.manager.customers_list.CustomersListPage import CustomersListPage
 from pages.manager.ManagerPage import ManagerPage
+import allure
 
 class TestDeleteCust():
+    @allure.description('Test deleting a customer based on name length')
+    @allure.feature('Delete customer')
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_delete_cust(self, setup):
         customers_list_page = CustomersListPage(setup)
         manager_page = ManagerPage(setup)
 
-        manager_page.click_customers()
-        assert manager_page.is_endpoint('list')
+        with allure.step('Navigate to customers list page'):
+            manager_page.click_customers()
+            assert manager_page.is_endpoint('list')
 
-        target_name = customers_list_page.calc_name_for_deletion()
-        customers_list_page.search(target_name)
-        assert customers_list_page.is_found(target_name)
+        with allure.step('Search for a name based on math condition'):
+            target_name = customers_list_page.calc_name_for_deletion()
+            customers_list_page.search(target_name)
+            assert customers_list_page.is_found(target_name)
 
-        customers_list_page.delete_cust()
-        assert customers_list_page.is_cust_deleted(target_name)
+        with allure.step('Hit delete button'):
+            customers_list_page.delete_cust()
+            assert customers_list_page.is_cust_deleted(target_name)
         
--- tests/test_sort_customers_by_name.py
+++ tests/test_sort_customers_by_name.py
@@ -1,14 +1,20 @@
 from pages.manager.ManagerPage import ManagerPage
 from pages.manager.customers_list.CustomersListPage import CustomersListPage
+import allure
 
 class TestSortCustomersByName:
+    @allure.description('Test sorting customers in ascending order')
+    @allure.feature('Sort customers in ascend order')
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_sort_customers_by_name(self, setup):
         manager_page = ManagerPage(setup)
         customers_list_page = CustomersListPage(setup)
 
-        manager_page.click_customers()
-        assert manager_page.matches_endpoint('list')
+        with allure.step('Navigate to customers list page'):
+            manager_page.click_customers()
+            assert manager_page.matches_endpoint('list')
 
-        customers_list_page.sort_by_name()
-        assert customers_list_page.is_sorted_ascen()
+        with allure.step('Sort by name in ascend order'):
+            customers_list_page.sort_by_name()
+            assert customers_list_page.is_sorted_ascen()
     
