6d7e66d878b56f4eb33711eb3d4ad5fcf5e0b7a3
veraci0us <200478683+veraci0us@users.noreply.github.com>
2025-04-01 15:34:16 +0300

    refactor helper funcs

--- helpers/math.py
+++ helpers/math.py
@@ -1,16 +1,11 @@
 def count_letters(words):
-    letters_in_words = []
-
-    for word in words:
-        letters_in_words.append(int(len(word)))
-
-    return letters_in_words
+    return [len[word] for word in words]
 
 def calc_average(letters_in_words):
     return round(sum(letters_in_words) / len(letters_in_words))
 
-def find_closest_index(list_of_nums, num):
-    diffs = [abs(num - number) for number in list_of_nums]
+def find_closest_index(list_of_nums, average):
+    diffs = [abs(average - number) for number in list_of_nums]
     smallest = min(diffs)
 
     return diffs.index(smallest)
--- helpers/turn_digit_to_letter.py
+++ helpers/turn_digit_to_letter.py
@@ -1,22 +1,13 @@
 import string
-import math
 
 def turn_digit_to_letter(post_code):
     alphabet = list(string.ascii_lowercase)
-    pairs = []
+    post_code = str(post_code)
     first_name = ''
    
-    for i in range(0, len(str(post_code)), 2):
-        pairs.append(str(post_code)[i:i+2])
-
-    for i in range(0, len(pairs)):
-        digit = int(pairs[i])
-        letters_num = len(alphabet)
-        if digit < letters_num:
-            first_name += alphabet[digit]
-        else: 
-            circle = math.floor(digit / letters_num)
-            index = digit - (circle * letters_num)
-            first_name += alphabet[index]
+    for i in range(0, len(post_code), 2):
+        pair = int(post_code[i: i+2])
+        index = pair % len(alphabet)
+        first_name += alphabet[index]
 
     return first_name
