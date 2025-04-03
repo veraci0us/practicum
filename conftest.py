6d3f752ff2d5715ec3001e010f4e4a648a0af5c8
veraci0us <200478683+veraci0us@users.noreply.github.com>
2025-04-03 18:17:27 +0300

    refactor conftest

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
 import os
-from dotenv import load_dotenv
 from helpers.faker_helper import gen_user_agent
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,8 +8,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from config import URL, PROXY_URL, TIMEOUT
+from pages.manager.ManagerPage import ManagerPage
+from pages.manager.add_customer.AddCustPage import AddCustPage
+from pages.manager.customers_list.CustomersListPage import CustomersListPage
 
-load_dotenv()
 user_agent = gen_user_agent()
 
 def is_github_actions():
@@ -23,9 +25,21 @@ def run_on_github():
     options.add_argument('--no-sandbox')
 
     driver = webdriver.Chrome(options=options)
-    driver.get(os.getenv('URL'))
+    driver.get(URL)
     
     return driver
+
+@pytest.fixture()
+def manager_page(setup):
+   return ManagerPage(setup)
+
+@pytest.fixture()
+def add_cust_page(setup):
+    return AddCustPage(setup)
+
+@pytest.fixture()
+def customers_list_page(setup):
+    return CustomersListPage(setup)
 
 @pytest.fixture(scope="function")
 def setup():
@@ -36,15 +50,14 @@ def setup():
         options = webdriver.ChromeOptions()
         options.add_argument(f'--user-agent: {user_agent}')
 
-        service = Service(ChromeDriverManager.install())
+        service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(options=options, service=service)
-        
+
         driver.delete_all_cookies()
 
-    driver.get(os.getenv('PROXY_URL'))
-
-    input = WebDriverWait(driver=driver, timeout=10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="url"]')))
-    input.send_keys(os.getenv('URL'))
+    driver.get(PROXY_URL)
+    input = WebDriverWait(driver=driver, timeout=TIMEOUT).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="url"]')))
+    input.send_keys(URL)
     input.send_keys(Keys.ENTER)
  
     driver.maximize_window()
